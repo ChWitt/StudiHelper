ddc0cf054ed6ac3f0a347447804b7afbd32da227
ChWitt <christoph.witting@googlemail.com>
2014-04-29 17:25:22 +0200

    UnitTest
    Add default UnitTest for DataAccess

---
+++ StudiHelper.Test/DataAccess/DataSourceConnectionTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudiHelper.Core.DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace StudiHelper.Core.DataAccess.Tests
+{
+  [TestClass()]
+  public class DataSourceConnectionTests
+  {
+    [TestMethod()]
+    public void GetConnectionTest()
+    {
+      // NOP
+    }
+
+    [TestMethod()]
+    public void GetModulesTest()
+    {
+      Assert.Fail();
+    }
+
+    [TestMethod()]
+    public void GetCursesByModulTest()
+    {
+      Assert.Fail();
+    }
+
+    [TestMethod()]
+    public void GetCursesTest()
+    {
+      Assert.Fail();
+    }
+
+    [TestMethod()]
+    public void GetMergedLessonsOfDayTest()
+    {
+      Assert.Fail();
+    }
+
+    [TestMethod()]
+    public void GetMergedLessonsOfWeekTest()
+    {
+      Assert.Fail();
+    }
+
+    [TestMethod()]
+    public void GetMergedLessonsOfCursTest()
+    {
+      Assert.Fail();
+    }
+  }
+}
